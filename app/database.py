ddc3d947428af7d973e405e6192235d039f752b1
sidnei48 <fariasidnei48@gmail.com>
2026-06-10 17:41:03 -0300

    Implementado configuração segura com variáveis de ambiente

--- app/database.py
+++ app/database.py
@@ -1,13 +1,19 @@
+import os
 import psycopg
 
-def get_connection():
+from dotenv import load_dotenv
 
+#carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+def get_connection():
     """Função para obter uma conexão com o banco de dados PostgreSQL."""
+    
     return psycopg.connect(
-        host="localhost",
-        port=5432,
-        dbname="clima",
-        user="postgres",
-        password="junior48",
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
         client_encoding="utf-8"
     )
